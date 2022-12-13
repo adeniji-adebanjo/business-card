34d8b62a8a4566731cfa2ba7bc9928a657a1e5d1
Adebanjo Adeniji <aeben.adebanjo@gmail.com>
2022-12-14 00:59:59 +0100

    adjusted the image and dset buttons right

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { FaBeer, FaLinkedin, FaLinkedinIn } from "react-icons/fa";
 
 export default function Buttons() {
   return (
     <>
       <button className="email-btn">
-        <span>
-          <i fa fa-envelope></i>
-        </span>
-        Email
+        <a href="mailto:aeben.adebanjo@gmail.com">
+          <span>
+            <FaBeer />
+          </span>
+          Email
+        </a>
       </button>
-      <button className="linkedin-btn">LinkedIn</button>
+      <button className="linkedin-btn">
+        <a href="https://linkedin.com/inadebanjo-adeniji">
+          <span>
+            <FaLinkedin />
+          </span>
+          LinkedIn
+        </a>
+      </button>
     </>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,25 +12,29 @@ export default function Footer() {
     <footer>
       <div className="social-container">
         <a
-          href="https://www.youtube.com/apluscodemaster"
+          href="https://www.youtube.com/@apluscodemaster"
+          target="_blank"
           className="youtube social"
         >
           <FontAwesomeIcon icon={faYoutube} size="2x" />
         </a>
         <a
           href="https://www.facebook.com/apluscodemaster"
+          target="_blank"
           className="facebook social"
         >
           <FontAwesomeIcon icon={faFacebook} size="2x" />
         </a>
         <a
           href="https://www.twitter.com/apluscodemaster"
+          target="_blank"
           className="twitter social"
         >
           <FontAwesomeIcon icon={faTwitter} size="2x" />
         </a>
         <a
           href="https://www.instagram.com/apluscodemaster"
+          target="_blank"
           className="instagram social"
         >
           <FontAwesomeIcon icon={faInstagram} size="2x" />
